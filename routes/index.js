5898d6f617fef8154929902c267c8c5726ce815f
juanfcomartin <juanfcomartin@gmail.com>
2015-07-06 22:28:02 +0200

    P2P: página de créditos

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author info */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Sobre el autor' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
