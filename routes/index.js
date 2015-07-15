7f78dbd6c1f826e7df68831a498bbaadffc3204d
juanfcomartin <juanfcomartin@gmail.com>
2015-07-15 12:34:18 +0200

    Modulo 7. Autoload

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,8 @@ router.get('/author', function(req, res) {
   res.render('author', { title: 'Sobre el autor' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
